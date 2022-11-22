3c1dd5e969dd4d94763092c657d88d208f9af306
SontiNagaSruthi <105856869+SontiNagaSruthi@users.noreply.github.com>
2022-11-22 13:23:51 +0530

    Add files via upload

--- Java/Activities/Activity1.java
+++ Java/Activities/Activity1.java
@@ -2,7 +2,14 @@ package activities;
 
 public class Activity1 {
     public static void main(String[] args) {
-        Car obj = new Car();
-        System.out.println(obj.name);
+        Car toyota = new Car();
+        toyota.make = 2014;
+        toyota.color = "Black";
+        toyota.transmission = "Manual";
+
+        //Using Car class method
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
     }
 }

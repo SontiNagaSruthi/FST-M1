292a000d925ddb5dfe855eefa010fece40c649aa
SontiNagaSruthi <105856869+SontiNagaSruthi@users.noreply.github.com>
2022-11-22 13:28:36 +0530

    Add files via upload

---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,7 @@
+UPDATE salesman SET grade=200 where salesman_city='Rome';
+UPDATE salesman SET grade=300 where salesman_name='James Hoog';
+UPDATE salesman SET salesman_name='Pierre' where salesman_name='McLyon';
+
+//displays modified data
+
+SELECT * FROM salesman;
